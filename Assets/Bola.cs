c61606f72da8f76e8939a2b099ff3a91246c76b6
rec16005 <rec16005@uvg.edu.gt>
2017-04-01 21:48:38 -0600

    Bola

---
+++ Assets/Bola.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bola : MonoBehaviour {
+	
+	private Rigidbody2D rb2d;
+    private Vector2 vel;
+	
+	
+	void GoBall()
+    {
+        float rand = Random.Range(0.0f, 2.0f);
+        if (rand < 1.0f)
+        {
+            rb2d.AddForce(new Vector2(0.0f, -20.0f));
+        }
+        else
+        {
+            rb2d.AddForce(new Vector2(0.0f, -20.0f));
+        }
+    }
+	
+	void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.collider.CompareTag("Player"))
+        {
+            vel.x = rb2d.velocity.x;
+            vel.y = (rb2d.velocity.y / 2.0f) + (coll.collider.attachedRigidbody.velocity.y / 3.0f);
+            rb2d.velocity = vel;
+        }
+    }
+	
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D>();
+        Invoke("GoBall", 2.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Pad.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pad : MonoBehaviour {
+	
+	Vector3 padTras;
+    float vel;
+
+	// Use this for initialization
+	void Start () {
+		padTras = new Vector3(0.0f, 0.0f, 0.0f);
+        vel = 10.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		padTras.x = 0.0f;
+        padTras.y = 0.0f;
+        padTras.z = 0.0f;
+		
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			padTras.y = -vel * Time.deltaTime;
+			transform.Translate(padTras);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			padTras.y = vel * Time.deltaTime;
+			transform.Translate(padTras);
+		}
+		
+	}
+}
